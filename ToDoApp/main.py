34f226f49b36955c0f8290a39fc8eaaee60b5155
Javier Martinez <javi.martinez097@gmail.com>
2024-10-28 16:12:19 -0600

    feat: :sparkles: Day 14: Modules

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,11 +1,4 @@
-def get_todos(filepath = './ToDoApp/todos.txt'):
-    with open(filepath,'r') as file:
-        todos_local = file.readlines()
-    return todos_local
-
-def write_todos(todos_arg,filepath = './ToDoApp/todos.txt'):
-        with open(filepath,'w') as file:
-            file.writelines(todos_arg)
+from functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete and exit: ")
