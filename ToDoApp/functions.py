c73e0b6525fa5c5b9a482b248ae114f8757a7d23
Javier Martinez <javi.martinez097@gmail.com>
2024-10-28 16:26:57 -0600

    feat: :sparkles: DAY 15: standard modules
    We add a time function

--- ToDoApp/functions.py
+++ ToDoApp/functions.py
@@ -1,8 +1,10 @@
-def get_todos(filepath = './ToDoApp/todos.txt'):
+FILEPATH = './ToDoApp/todos.txt'
+
+def get_todos(filepath = FILEPATH):
     with open(filepath,'r') as file:
         todos_local = file.readlines()
     return todos_local
 
-def write_todos(todos_arg,filepath = './ToDoApp/todos.txt'):
+def write_todos(todos_arg,filepath = FILEPATH):
         with open(filepath,'w') as file:
             file.writelines(todos_arg)
--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,4 +1,8 @@
-from functions import get_todos, write_todos
+from functions import *
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is "+now)
 
 while True:
     user_action = input("Type add, show, edit, complete and exit: ")
