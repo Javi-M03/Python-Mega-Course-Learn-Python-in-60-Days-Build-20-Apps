9a1954c3afc4a26553497d2755c9b019d20bb5a0
Javier Martinez <javi.martinez097@gmail.com>
2024-11-01 09:43:06 -0600

    feat: :sparkles: Day 26: excel to PDF Invoice Generator pt2

--- 4 - Invoice Generator/StudentProject/main.py
+++ 4 - Invoice Generator/StudentProject/main.py
@@ -1,4 +1,3 @@
-import pandas
 from fpdf import FPDF
 import glob
 from pathlib import Path
@@ -7,13 +6,16 @@ filepaths = glob.glob("textfiles/*.txt")
 pdf =  FPDF(orientation="P",unit="mm",format="A4")
 
 
-for filepath in filepaths:
-    df = pandas.read_csv(filepath)
+for index,filepath in enumerate(filepaths):
     pdf.add_page()
     filename = Path(filepath).stem.capitalize()
     pdf.set_font(family="Times",size=24,style="B")
-    pdf.cell(w=50,h=8,txt=f"{filename}")
-    pdf.set_font(family="Times",size=12,style="B")
-    pdf.cell(w=50,h=8,txt=f"{df}")
+    pdf.cell(w=50,h=8,txt=f"{filename}",ln=1)
+    pdf.set_font(family="Times",size=10)
+
+    #Content
+    with open(filepath) as file:
+        content = file.read()
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output(f"PDFs/output.pdf")
