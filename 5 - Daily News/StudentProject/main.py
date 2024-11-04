1dbeeea5105dd4f3bb368e0fcd1ee08da83ff58c
Javier Martinez <javi.martinez097@gmail.com>
2024-11-04 09:13:56 -0600

    feat: :sparkles: Day 28: Email News from Email  API pt 2/ Student Project NASA API

--- 5 - Daily News/StudentProject/main.py
+++ 5 - Daily News/StudentProject/main.py
@@ -1,19 +1,18 @@
 import requests
-from send_email import send_email
+import streamlit as st
 
-api_key = "418cfcb3b4314098a6358ce488664b2f"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-10-04&sortBy=publishedAt&apiKey=418cfcb3b4314098a6358ce488664b2f"
 
-#Make a request
+api_key = "4G0dQ2JVfxEE1D6cejkfAkmKqdJ5fmwWDIzBJHfJ"
+url = "https://api.nasa.gov/planetary/apod?api_key=4G0dQ2JVfxEE1D6cejkfAkmKqdJ5fmwWDIzBJHfJ"
+
+
+
 request = requests.get(url)
 
 #Get a dictionary with data
 content = request.json()
 
-message = ""
 
-#Access the article title and description
-for article in content["articles"]:
-    message = message + str(article["title"]) + "\n" + article["description"] + 2*'\n'
-message = message.encode("utf-8")
-send_email(message)
+st.title(content["title"])
+st.image(content["url"])
+st.write(content["explanation"])
--- 5 - Daily News/main.py
+++ 5 - Daily News/main.py
@@ -1,7 +1,12 @@
 import requests
+from send_email import send_email
 
 api_key = "418cfcb3b4314098a6358ce488664b2f"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-10-04&sortBy=publishedAt&apiKey=418cfcb3b4314098a6358ce488664b2f"
+url = "https://newsapi.org/v2/everything?"\
+        "q=tesla&"\
+        "from=2024-10-04&"\
+        "sortBy=publishedAt&apiKey=418cfcb3b4314098a6358ce488664b2f&"\
+        "language=en"
 
 #Make a request
 request = requests.get(url)
@@ -9,7 +14,15 @@ request = requests.get(url)
 #Get a dictionary with data
 content = request.json()
 
+message = ""
+
 #Access the article title and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        message = "Subject: Today's News" \
+        +"\n"+ message + article["title"] + "\n" \
+        + article["description"] \
+        + '\n' + article["url"] + 2*'\n'
+        
+message = message.encode("utf-8")
+send_email(message)
