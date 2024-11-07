06108ecbb7c5e82aa092828de0dcd24feb081487
Javier Martinez <javi.martinez097@gmail.com>
2024-11-07 12:13:17 -0600

    feat: :sparkles: Day 30: Weather app pt2 improving the API call

--- 6 - Weather App/StudentProject/main.py
+++ 6 - Weather App/StudentProject/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+df = pd.read_csv("dictionary.csv")
 
 @app.route("/")
 def home():
@@ -8,7 +10,7 @@ def home():
 
 @app.route("/api/v1/<word>/")
 def about(word):
-    definition = word.upper()
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
     return {"word":word,
             "definition": definition}
 
