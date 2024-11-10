5c8ac8488cd5b0ccaa7c177ff886150120afef59
Javier Martinez <javi.martinez097@gmail.com>
2024-11-10 11:46:30 -0600

    feat: :sparkles: Day 33: Forecast weather app:  Connect to API an show temperature and sky forecast

---
+++ 7 - Weather Forecast App/backend.py
@@ -0,0 +1,15 @@
+import credentiales
+import requests
+
+def get_data(place,forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={credentiales.apiKey}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo",forecast_days=3))
