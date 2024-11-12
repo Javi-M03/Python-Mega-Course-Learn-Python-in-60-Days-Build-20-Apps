3db124e83bda71822935eb70703175efe8f22c67
Javier Martinez <javi.martinez097@gmail.com>
2024-11-12 10:38:29 -0600

    feat: :sparkles: Day 37: WebApp alert final
    Threading and sending image in email

--- 9 - Webcam Alert/emailing.py
+++ 9 - Webcam Alert/emailing.py
@@ -1,2 +1,27 @@
-def send_email():
-    print("Email Was Sent")
+import smtplib, imghdr
+from email.message import  EmailMessage
+from credentiales import SENDER,PASSWORD
+
+
+def send_email(image_path):
+    print("Send Email Start")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Customer showed up!"
+    email_message.set_content("Hey! we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    
+    email_message.add_attachment(content,maintype='image', subtype=imghdr.what(None,content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+
+    #Sender, Reciever, message
+    gmail.sendmail(SENDER, SENDER, email_message.as_string())
+
+    print("Send email ended")
+if __name__ == "__main__":
+    send_email(image_path="images/36.png")
--- 9 - Webcam Alert/main.py
+++ 9 - Webcam Alert/main.py
@@ -1,5 +1,6 @@
-import cv2,time
+import cv2,time,glob,os
 from emailing import send_email
+from threading import Thread
 
 
 video = cv2.VideoCapture(0)
@@ -7,6 +8,15 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+def clean_folder():
+    print("Clean start")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("Clean Ended")
+
 while True:
     status = 0
     check, frame = video.read()
@@ -31,13 +41,22 @@ while True:
         rectangle =  cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
         if rectangle.any:
             status = 1
+            cv2.imwrite(f"images/{count}.png",frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target = send_email, args=(image_with_object,))
+        email_thread.daemon = True
+        clean_thread = Thread(target = clean_folder)
 
+        email_thread.start()
+        
     
     cv2.imshow("Video",frame)
 
@@ -48,3 +67,5 @@ while True:
         break
 
 video.release()
+clean_thread.start()
+
